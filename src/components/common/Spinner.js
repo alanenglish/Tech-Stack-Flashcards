2a5a0809dfbfd8bc2736c5cd17cf442d9735242e
Alan English <aenglish@manorhillcapital.com>
2017-09-17 16:52:49 -0500

    add styles to spinner component

--- src/components/common/Spinner.js
+++ src/components/common/Spinner.js
@@ -10,4 +10,12 @@ const Spinner = ({ size }) => {
   );
 };
 
+const styles = {
+  spinnerStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+};
+
 export { Spinner };
