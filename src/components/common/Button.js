09abe7060bac7dc0e16ed89a3f8cbe2d61e7cd64
Alan English <aenglish@manorhillcapital.com>
2017-09-17 16:41:55 -0500

    create components folder, common components folder, button functional component

---
+++ src/components/common/Button.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+const Button = ({ onPress, children }) => {
+  const { buttonStyle, textStyle } = styles;
+
+  return (
+    <TouchableOpacity onPress={onPress} style={buttonStyle}>
+      <Text style={textStyle}>
+        {children}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export { Button };
