b180981701e20ab354fc4ec18e63bf88c2f651e7
Alan English <aenglish@manorhillcapital.com>
2017-09-17 16:48:33 -0500

    add functional header component

---
+++ src/components/common/Header.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+};
+
+export { Header };
