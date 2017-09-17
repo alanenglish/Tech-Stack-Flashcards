1712eab181f723ac89e260eee676a4a252d8a550
Alan English <aenglish@manorhillcapital.com>
2017-09-17 16:44:15 -0500

    add card section functional component

---
+++ src/components/common/CardSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const CardSection = (props) => {
+  return (
+    <View style={styles.containerStyle}>
+      {props.children}
+    </View>
+  );
+};
+
+export { CardSection };
